1c1edb1f7548984dd20f98214e82faeea9db6855
Racz Balazs <diak_243.racz.balazs.zsolt@11.c.2026.com>
2025-12-11 01:46:07 +0100

    login has been expanded with errormessages, and axios posts the form's data

--- frontend/src/pages/login.vue
+++ frontend/src/pages/login.vue
@@ -11,6 +11,25 @@ import {
 } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import {ref} from 'vue'
+import axiosClient from '@utils/http.mjs'
+
+const data = ref({
+    email: '',
+    password: ''
+})
+const errorMessage = ref('')
+
+function submit(){
+    axiosClient.get('sanctum/csrf-cookie').then(response => {
+        axiosClient.post('/login', data.value).then(response => {
+            router.push({name: Home})
+        }).catch(error => {
+            console.log(error.response)
+            errorMessage.value = error.response.data.message
+        })
+    })
+}
 </script>
 <template>
     <section class="bg-primary h-[100vh] flex flex-col">
@@ -22,12 +41,15 @@ import { Label } from '@/components/ui/label'
                     Enter your email below to login to your account
                 </CardDescription>
                 <CardAction>
-                    <Button variant="link" to="/register">
+                    <Button variant="link" to="/signup">
                     Sign Up
                     </Button>
                 </CardAction>
                 </CardHeader>
                 <CardContent>
+                    <div v-if="errorMessage" class="mt-4 py-2 px-3 rounded text-white bg-red-400">
+                    {{errorMessage}}
+                    </div>
                 <form>
                     <div class="grid w-full items-center gap-4">
                     <div class="flex flex-col space-y-1.5">
@@ -42,7 +64,7 @@ import { Label } from '@/components/ui/label'
                 </form>
                 </CardContent>
                 <CardFooter class="flex flex-col gap-2">
-                <Button class="w-full" to="/">
+                <Button class="w-full" @click="submit">
                     Login
                 </Button>
                 </CardFooter>
@@ -50,8 +72,3 @@ import { Label } from '@/components/ui/label'
         </div>
   </section>
 </template>
-<route lang="yaml">
-name: login
-meta:
-    title: Login
-</route>
