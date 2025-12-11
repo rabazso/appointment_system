b469418b3e5226c97181db6a66f76149f5c8ec2a
Racz Balazs <diak_243.racz.balazs.zsolt@11.c.2026.com>
2025-12-11 01:23:44 +0100

    autoroute removed, routes moved into the router/index.js

--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -1,17 +1,8 @@
 <script setup>
 import BaseLayout from '@layouts/BaseLayout.vue'
-import { useCounter } from '@stores/CounterStore.mjs'
-
-const counter = useCounter()
 </script>
 
 <template>
   <BaseLayout>
   </BaseLayout>
 </template>
-
-<route lang="yaml">
-name: home
-meta:
-  title: Home
-</route>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { setTitle } from '@/router/guards/SetTitleGuard.mjs'
-import { routes } from 'vue-router/auto-routes'
+import Index from '@pages/index.vue'
+import Login from '@pages/login.vue'
+import Register from '@pages/signup.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Index,
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+  },
+  {
+    path: '/signup',
+    name: 'SignUp',
+    component: Register,
+  },
+]
 
 export const router = createRouter({
   history: createWebHistory(),
