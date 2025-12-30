03dbcca618e3bdd00013563b56e197aae9b58ff1
Gáspár Mihály <gaspar.mihaly.11@gmail.com>
2025-12-30 11:28:08 +0100

    making of the contact page

---
+++ frontend/src/components/layout/ContactLayout.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1 class="text-2xl text-center font-bold mb-4 mt-4">Contact</h1>
+        <p class="flex items-start mb-2">
+            <span class="mr-2">📍</span>Budapest 1234 Barber utca 67
+        </p>
+        <p class="flex items-center mb-2">
+            <span class="mr-2">📞</span> 012345678
+        </p>
+        <p class="flex items-center mb-2">
+            <span class="mr-2">✉︎</span> barber@shop.com
+        </p>
+        <div class="mt-4">
+            <p class="flex items-start">
+                <span class="mr-2">🕒</span>Mon-Fri: 9am-8pm
+            </p>
+            <p class="ml-6">
+                Sat: 10am-5pm
+            </p>
+            <p class="ml-6">
+                Sun: closed
+            </p>
+        </div>
+    </div>
+</template>
--- frontend/src/pages/contact.vue
+++ frontend/src/pages/contact.vue
@@ -1,8 +1,10 @@
 <script setup>
 import BaseHeader from '@components/layout/BaseHeader.vue';
+import ContactLayout from '@components/layout/ContactLayout.vue';
 
 
 </script>
 <template>
 <BaseHeader/>
+<ContactLayout/>
 </template>
