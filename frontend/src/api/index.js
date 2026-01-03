91e5e55f43f3d63bfc6d06ffa585d412f85bd7e0
Bertalan Toth <bertalan.toth.neu@gmail.com>
2026-01-03 16:05:07 +0100

    Created the method for using tokens for requests in axios

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -4,10 +4,18 @@ const API = axios.create({
     baseURL: 'http:backend.vm1.test',
 });
 
+export const setAuthToken = (token) => {
+    if (token) {
+        API.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    } else {
+        delete API.defaults.headers.common['Authorization'];
+    }
+};
+
 export const getServices = () => API.get('/services');
 export const getEmployees = () => API.get('/employees');
 export const getEmployeesByService= (serviceId) => API.get(`/employees?service_id=${serviceId}`);
 export const getAppointmentByService = (serviceId) => API.get(`/appointments?service_id=${serviceId}`);
 export const getEmployeesByServiceAndAppointment = (serviceId, appointment) => API.get(`/employees?service_id=${serviceId}&appointment=${appointment}`);
 export const getAppointmentsByServiceAndEmployee = (serviceId, employeeId) => API.get(`/appointments?service_id=${serviceId}&employee_id=${employeeId}`);
-export const getReviews = () => API.get('/reviews')
+export const getReviews = () => API.get('/reviews');
