7b1fa9ca5762c1b62d20cbd103b4133ea35ad1c4
Bertalan Toth <bertalan.toth.neu@gmail.com>
2026-01-03 12:37:21 +0100

    Refactored employee calculation

---
+++ backend/app/Calculations/EmployeeCalculation.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Calculations;
+
+use App\Models\Service;
+use App\Models\Employee;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class EmployeeCalculation
+{
+    public function employees(Request $request): array
+    {
+        $serviceId = $request->get('service_id');
+
+        $serviceDuration = Service::findOrFail($serviceId)->duration;
+
+        $appointmentStart = Carbon::parse($request->get('appointment_start'));
+        $slotStart = $appointmentStart->copy();
+        $slotEnd   = $appointmentStart->copy()->addMinutes($serviceDuration);
+
+        $employees = Employee::query()
+            ->whereHas('services', fn ($x) =>
+                $x->where('services.id', $serviceId)
+            )
+            ->whereDoesntHave('appointments', fn ($x) =>
+                $x->where('start_datetime', '<', $slotEnd)
+                  ->where('end_datetime', '>', $slotStart)
+            )
+            ->get();
+
+        return [
+            'employees' => $employees,
+        ];
+    }
+}
