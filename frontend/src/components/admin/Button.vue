cdc9c5bcef4cdf90285d19ccb6e59fe75260aa64
Bertalan Toth <bertalan.toth.neu>
2026-03-06 18:00:52 +0100

    Created button component for admin interface

---
+++ frontend/src/components/admin/Button.vue
@@ -0,0 +1,16 @@
+<template>
+  <button :type="type"
+    class="inline-flex items-center justify-center whitespace-nowrap rounded-md bg-secondary px-4 py-2 text-base font-semibold text-primary transition duration-100 hover:bg-secondary-100 shadow-sm hover:shadow-lg active:translate-y-px"
+    v-bind="$attrs">
+    <slot />
+  </button>
+</template>
+
+<script setup>
+defineProps({
+  type: {
+    type: String,
+    default: 'button'
+  }
+})
+</script>
