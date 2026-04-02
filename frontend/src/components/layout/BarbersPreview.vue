153d0f72a732b3b74760782a9f964086d3edcb9b
balazsRacz <balazs.racz06@gmail.com>
2026-04-02 11:55:44 +0200

    refine section heights on index

--- frontend/src/components/layout/BarbersPreview.vue
+++ frontend/src/components/layout/BarbersPreview.vue
@@ -77,7 +77,7 @@ onUnmounted(() => {
 })
 </script>
 <template>
-    <section id="barbers" class="py-15 bg-background scroll-mt-15">
+    <section id="barbers" class="py-15 bg-background scroll-mt-15 xl:h-[94vh]">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div class="text-center mb-16">
                 <h2 class="text-4xl md:text-5xl font-bold mb-4">Meet Our Barbers</h2>
--- frontend/src/components/layout/LandingReview.vue
+++ frontend/src/components/layout/LandingReview.vue
@@ -136,14 +136,14 @@ watch(isLoggedIn, (loggedIn) => {
         <div class="grid md:grid-cols-3 gap-8">
             <Card v-for="review in reviews" :key="review?.id" class="border-2 border-border py-8 px-5 rounded-lg">
                 <CardContent class="space-y-1.5 flex flex-col">
-                    <CardTitle class="text-lg font-semibold text-primary mb-1">{{ review.user?.name }}</CardTitle>
+                    <CardTitle class="text-lg font-semibold text-primary mb-1">{{ review.customer?.name }}</CardTitle>
                     <CardDescription class="flex items-center gap-1 flex-nowrap"><Star v-for="star in review.rating" :key="star" class="w-4 h-4 text-primary fill-accent"></Star></CardDescription>
                     <CardDescription class="text-accent font-semibold text-xl min-h-6">{{ review.comment || '' }}</CardDescription>
                 </CardContent>
             </Card>
         </div>
         <div class="text-center mt-50">
-            <Button variant="outlinebackground" class="text-lg md:text-xl font-bold w-fit px-20 py-8" @click="openReviewModal">
+            <Button variant="default" class="text-lg md:text-xl font-bold w-fit px-20 py-8 mt-24" @click="openReviewModal">
                 Leave a review
             </Button>
         </div>
