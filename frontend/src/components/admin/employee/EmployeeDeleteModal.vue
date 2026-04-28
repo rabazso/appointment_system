e6583356e5d03abab982021d0b353001c40fc47a
Bertalan <bertalan.toth.neu@gmail.com>
2026-04-28 15:49:22 +0200

    Changed ui in delete modals

--- frontend/src/components/admin/employee/EmployeeDeleteModal.vue
+++ frontend/src/components/admin/employee/EmployeeDeleteModal.vue
@@ -1,39 +1,15 @@
 <template>
-  <ModalShell content-class="max-w-sm" @close="$emit('close')">
-    <ModalHeader
-      title="Delete employee"
-      description="This will also remove the employee account and all related configuration records."
-    />
-
-    <div class="space-y-4">
-      <p class=" leading-6 text-center">
-        Are you sure you want to delete <br>
-        <span class="font-semibold">{{ employee.name }}</span>?
-      </p>
-
-      <div class="flex justify-end gap-3">
-        <button
-          type="button"
-          class="inline-flex py-2 items-center justify-center rounded-xl border border-black/10 bg-white px-4 text-sm font-medium"
-          @click="$emit('close')"
-        >
-          Cancel
-        </button>
-        <button
-          type="button"
-          class="inline-flex py-2 items-center justify-center rounded-xl bg-secondary px-4 text-sm font-medium text-black"
-          @click="$emit('confirm')"
-        >
-          Confirm
-        </button>
-      </div>
-    </div>
-  </ModalShell>
+  <ConfirmDeleteModal
+    title="Delete employee"
+    description="This will also remove the employee account and all related configuration records."
+    :target-name="employee.name"
+    @close="$emit('close')"
+    @confirm="$emit('confirm')"
+  />
 </template>
 
 <script setup>
-import ModalHeader from '@/components/admin/ModalHeader.vue'
-import ModalShell from '@/components/admin/ModalShell.vue'
+import ConfirmDeleteModal from '@/components/admin/ConfirmDeleteModal.vue'
 
 defineEmits(['close', 'confirm'])
 
